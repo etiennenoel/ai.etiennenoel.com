87444fa8d545a4bc2181c1d2f682d8b42ea5e825
Etienne Noel <etiennenoel@gmail.com>
2025-02-26 19:23:14 -0800

    - Update

--- src/app/enums/availability-status.enum.ts
+++ src/app/enums/availability-status.enum.ts
@@ -1,6 +1,14 @@
 export enum AvailabilityStatusEnum {
+  /** @deprecated **/
   Readily = 'readily',
+  /** @deprecated **/
   AfterDownload = 'after-download',
+  /** @deprecated **/
   No = 'no',
+  /** @deprecated **/
   Unknown = 'unknown',
+  Unavailable = "unavailable",
+  Downloadable = "downloadable",
+  Downloading = "downloading",
+  Available = "available",
 }
