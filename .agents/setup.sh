995ec9cce278362f234f7a68e56fe42a1eebf22d
Etienne Noel <etiennenoel@gmail.com>
2025-08-21 08:46:06 -0700

    update

--- .agents/setup.sh
+++ .agents/setup.sh
@@ -5,7 +5,7 @@ current_file_location=$(dirname "$0")
 # Install ci-cd code
 cd ${current_file_location}/.. || exit
 
-echo "//npm.pkg.github.com/:_authToken=${NPM_READ_TOKEN}" >> .npmrc
+echo "//npm.pkg.github.com/:_authToken=${$1}" >> .npmrc
 
 npm ci
 
