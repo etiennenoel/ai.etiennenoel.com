e84a2c6f5f499b3b3c5ff78c1ab20a6094b8f036
Etienne Noel <etiennenoel@gmail.com>
2025-08-22 10:38:27 -0700

    update

--- projects/web-ai-studio/src/lib/web-ai-studio.module.ts
+++ projects/web-ai-studio/src/lib/web-ai-studio.module.ts
@@ -1,5 +1,17 @@
 import { NgModule } from '@angular/core';
 import {ChatPage} from './pages/chat/chat.page';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from '../../../../src/app/app-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MagienoDragAndDropComponent} from '@magieno/angular-drag-and-drop';
+import {MagienoBootstrapDropdownComponent} from '@magieno/angular-bootstrap-dropdown';
+import {MagienoCodeEditorModule} from '@magieno/angular-code-editor';
+import {MagienoCoreModule} from '@magieno/angular-core';
+import {MagienoMediaModule} from '@magieno/angular-media';
+import {MagienoAdvancedTableComponent} from '@magieno/angular-advanced-table';
+import {MagienoAIModule} from '@magieno/angular-ai';
+import {NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -8,6 +20,19 @@ import {ChatPage} from './pages/chat/chat.page';
     ChatPage,
   ],
   imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DragDropModule,
+    MagienoDragAndDropComponent,
+    MagienoBootstrapDropdownComponent,
+    MagienoCodeEditorModule,
+    MagienoCoreModule,
+    MagienoMediaModule,
+    MagienoAdvancedTableComponent,
+    MagienoAIModule,
+    NgbTooltip,
   ],
   exports: [
     ChatPage,
