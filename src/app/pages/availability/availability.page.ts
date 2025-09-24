64bf47f5f01517a599fffa1c7e9900d5250a2a89
Etienne Noel <etiennenoel@gmail.com>
2025-09-24 13:16:52 -0700

    update

--- src/app/pages/availability/availability.page.ts
+++ src/app/pages/availability/availability.page.ts
@@ -1,10 +1,11 @@
-import {Component, Inject, Input, OnInit, DOCUMENT} from '@angular/core';
+import {Component, Inject, Input, OnInit, DOCUMENT, PLATFORM_ID} from '@angular/core';
 import {ToastStore} from '../../stores/toast.store';
 
 import {ToastMessageInterface} from '../../interfaces/toast-message.interface';
 import {delay, pipe} from 'rxjs';
 import {BasePageComponent} from '../../components/base/base-page.component';
 import {Title} from '@angular/platform-browser';
+import {isPlatformServer} from '@angular/common';
 
 @Component({
   selector: 'page-availability',
@@ -14,11 +15,12 @@ import {Title} from '@angular/platform-browser';
 })
 export class AvailabilityPage extends BasePageComponent implements OnInit {
 
-  promptAvailability: 'loading' | 'downloadable' | 'unavailable' | 'available' | 'downloading' = 'downloadable'
+  promptAvailability: 'loading' | 'downloadable' | 'unavailable' | 'available' | 'downloading' = 'loading'
 
   promptError?: string;
 
   constructor(
+    @Inject(PLATFORM_ID) private readonly platformId: Object,
     @Inject(DOCUMENT) document: Document,
     title: Title,
   ) {
@@ -32,6 +34,10 @@ export class AvailabilityPage extends BasePageComponent implements OnInit {
   }
 
   async checkPromptAvailability() {
+    if(isPlatformServer(this.platformId)) {
+      return;
+    }
+
     try {
       // @ts-expect-error
       this.promptAvailability = await LanguageModel.availability();
